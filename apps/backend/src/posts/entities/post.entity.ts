e126b4f59857e0e0c9a80a576de1fb83ae5f7fd0
Julia Olshanska <yulyaolshanska@gmail.com>
2025-04-29 17:20:19 +0300

    feat: add  posts module

---
+++ apps/backend/src/posts/entities/post.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+
+import { Comment } from '../../comments/entities/comment.entity';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @OneToMany(() => Comment, (comment) => comment.post, { cascade: true })
+  comments: Comment[];
+}
---
+++ apps/backend/src/posts/posts.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+
+import { PostsService } from './posts.service';
+import { CreatePostDto,UpdatePostDto } from './dto/dto';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAll() {
+    return this.postsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.postsService.findOne(+id);
+  }
+
+  @Post()
+  create(@Body() dto: CreatePostDto) {
+    return this.postsService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdatePostDto) {
+    return this.postsService.update(+id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.postsService.remove(+id);
+  }
+}
