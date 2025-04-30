e36e37f356caaef6789a90bd64f171f05a7105c7
Julia Olshanska <yulyaolshanska@gmail.com>
2025-04-30 17:20:18 +0300

    feat: add pagination for posts, createdAt, updatedAt

--- apps/backend/src/posts/entities/post.entity.ts
+++ apps/backend/src/posts/entities/post.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 import { Comment } from '../../comments/entities/comment.entity';
 
@@ -12,6 +19,12 @@ export class Post {
 
   @Column('text')
   content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 
   @OneToMany(() => Comment, comment => comment.post, { cascade: true })
   comments: Comment[];
